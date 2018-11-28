90bfd4c55632b609f8e9a05e5516463b324a16ee
Jeremias Erbs <jerry@badabam.de>
2018-11-28 12:53:57 +0100

    Cleanup

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,9 @@ var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Nothing here. Check out other routes')
+  res.json({
+    foo: 'bar',
+  })
 })
 
 module.exports = router
