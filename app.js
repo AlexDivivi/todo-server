242caf6eb67049ea8e4ca7c38af61122d201b57a
Jeremias Erbs <jerry@badabam.de>
2018-11-29 08:49:52 +0100

    Add helmet with csp for security

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 var express = require('express')
 var app = express()
 
+require('./setup/setupSecurity')(app)
 require('./setup/setupCors')(app)
 require('./setup/setupDefaults')(app)
 require('./setup/setupRoutes')(app)
